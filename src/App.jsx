19682de5b2245b22d8898dc673927662e1a81c64
Lehron <lehronrobinson@yahoo.com>
2023-07-17 20:16:32 -0400

    final code for my movies app

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,119 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Movies from './Movies';
-import { useState, useRef } from 'react';
+import { useRef, useState } from "react";
+import Movies from "./Movies";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const nameRef = useRef();
+  const descRef = useRef();
+  const yearRef = useRef();
+  const genreRef = useRef();
 
- 
   function addMovie() {
-    console.log('got button click');
+    console.log("got button click");
     let currentNameText = nameRef.current.value || undefined;
-    setMovies([...movies, { name: currentNameText, desc: "description", year: "year", genre: "genre"}]);
+    let currentDescText = descRef.current.value || undefined;
+    let currentYearText = yearRef.current.value || undefined;
+    let currentGenreText = genreRef.current.value || undefined;
+
+    setMovies([
+      ...movies,
+      {
+        id: Date.now(),
+        name: currentNameText,
+        desc: currentDescText,
+        year: currentYearText,
+        genre: currentGenreText
+      }
+    ]);
+
+    nameRef.current.value = "";
+    descRef.current.value = "";
+    yearRef.current.value = "";
+    genreRef.current.value = "";
   }
 
-
-  let movieCollection = [
-    {
-      name: "Batman",
-      desc: "Dark Knight",
-      year: "2000",
-      genre: "action"
-    },
-    {
-      name: "Superman",
-      desc: "Man of Steel",
-      year: "20010",
-      genre: "action"
-    }
-  ];
+  function removeMovie(id) {
+    setMovies(
+      movies.filter((movie) => {
+        return movie.id !== id;
+      })
+    );
+  }
+  // let movieCollection = [
+  // 	{
+  // 		name: 'Batman',
+  // 		desc: 'Dark Knight',
+  // 		year: '2000',
+  // 		genre: 'action',
+  // 	},
+  // 	{
+  // 		name: 'Superman',
+  // 		desc: 'Man of Steel',
+  // 		year: '20010',
+  // 		genre: 'action',
+  // 	},
+  // ];
 
   return (
     <>
-      <div className="container board mt-3">
-        <div className="row text-center">
-          <h1>Movies List:</h1>
+      <div className='container-fluid board mt-3'>
+        <div className='row text-center'>
+          <h1>Movie List:</h1>
+          <span>Create your Movie Collection here!</span>
         </div>
-        <div className="row justify-center text-center">
-          {
-            movieCollection.map((movie, index) => {
-              return (
-                <Movies
-                    key= {index} 
-                    name= {movie.name}
-                    desc= {movie.desc} 
-                    year= {movie.year} 
-                    genre= {movie.genre} 
-                  />
-              )
-            })
-          }
-
-        </div>
-        <div className="row mt-3 d-flex justify-content-center">
-          <div className="col-md-6 ">
-            <div className="input-group mb-3">
-              <input
-                type="text"
-                ref= {nameRef}
-                className="form-control"
-                placeholder="Movie title here..."
-                aria-label="Movie Title"
+        <div className='row justify-content-center text-center'>
+          {movies.map((movie) => {
+            return (
+              <Movies
+                key={movie.id}
+                name={movie.name}
+                desc={movie.desc}
+                year={movie.year}
+                genre={movie.genre}
+                removeMovie={() => removeMovie(movie.id)}
               />
-              <div className="input-group-append">
+            );
+          })}
+        </div>
+        <div className='row mt-3 d-flex justify-content-center'>
+          <div className='col-md-9 '>
+            <div className='input-group m-3'>
+              <input
+                type='text'
+                ref={nameRef}
+                className='form-control m-1'
+                placeholder='Title'
+                aria-label='Movie Title'
+              />
+              <input
+                type='text'
+                ref={descRef}
+                className='form-control m-1'
+                placeholder='Description'
+                aria-label='Movie Description'
+              />
+              <input
+                type='text'
+                ref={yearRef}
+                className='form-control m-1'
+                placeholder='Release Year'
+                aria-label='Movie Release Year'
+              />
+              <input
+                type='text'
+                ref={genreRef}
+                className='form-control m-1'
+                placeholder='Genre'
+                aria-label='Movie Genre'
+              />
+              <div className='container mt-3 d-flex justify-content-center'>
                 <button
                   onClick={addMovie}
-                  className="btn btn-primary h-100 m-0"
-                  type="button"
+                  className='btn btn-primary h-100 m-0'
+                  type='button'
                 >
-                  Add
+                  Add Movie
                 </button>
               </div>
             </div>
--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,20 +1,39 @@
 import PropTypes from "prop-types";
-import './Movies.css';
+import { useState } from "react";
+import "./Movies.css";
 
-function Movies({ name, desc, year, genre }) {
+function Movies({ name, desc, year, genre, key, removeMovie }) {
+  // STATE FOR WATCH/UNWATCHED
+  const [watch, setWatch] = useState(false);
+
   return (
-    <div className="row gy-3">
-      <div className="col-md-8 movie m-auto">
-        <div className="row pt-3 text-box">
-          <p className="text title">{name}</p>
-          <p className="text">{desc}</p>
-          <p className="text">{year}</p>
-          <p className="text">{genre}</p>
+    <div className='row gy-3' key={key}>
+      <div className='col-md-8 movie m-auto'>
+        <div className='row pt-3 text-box'>
+          <p className='text title'>{name}</p>
+          <p className='text'>{desc}</p>
+          <p className='text'>{year}</p>
+          <p className='text'>{genre}</p>
         </div>
-        <div className="row">
-          <div className="col-md-2 p-0">
-            <button className="btn btn-danger w-75 px-0 m-0 my-1">
+        <div className='row'>
+          <div className='col-md-2 p-0'>
+            <button
+              className='btn btn-danger w-75 px-0 m-0 my-1'
+              onClick={removeMovie}
+            >
               Delete
+            </button>
+          </div>
+          <div className='col-md-2 p-0'>
+            <button
+              className={
+                watch
+                  ? "btn w-75 px-0 m-0 my-1 btn-success"
+                  : "btn w-75 px-0 m-0 my-1 btn-danger"
+              }
+              onClick={() => setWatch(!watch)}
+            >
+              {watch ? "Watched" : "Unwatched"}
             </button>
           </div>
         </div>
@@ -24,14 +43,16 @@ function Movies({ name, desc, year, genre }) {
 }
 
 Movies.defaultProps = {
-    text: "Untitled Movie"
-}
+  text: "Untitled Movie"
+};
 
 Movies.propTypes = {
   name: PropTypes.string.isRequired,
   desc: PropTypes.string.isRequired,
   year: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
+  key: PropTypes.string.isRequired,
+  removeMovie: PropTypes.isRequired
 };
 
 export default Movies;
